51a567bbe28d006927d44dd74cce9db4fbae08b5
Alexander Antonov <biz.showpozitif5@gmail.com>
2020-09-27 06:45:36 +0300

    Update db.js

--- assets/db.js
+++ assets/db.js
@@ -1,8 +1,8 @@
 ---
 ---
 
-const PROJECT = [];
-const SOCIAL = [];
+const PROJECT = {{ site.data.project | jsonify }};
+const SOCIAL = {{ site.data.social | jsonify }};
 
 globalThis._PROJECT = PROJECT;
 globalThis._SOCIAL = SOCIAL;
