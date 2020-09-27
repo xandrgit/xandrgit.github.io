d98e94b541fd5fed0f6758530fd90141431f8868
Alexander Antonov <biz.showpozitif5@gmail.com>
2020-09-27 06:42:52 +0300

    Update db.js

--- assets/db.js
+++ assets/db.js
@@ -1,8 +1,8 @@
 ---
 ---
 
-const PROJECT = [({{ site.data.project }}).replace(/=>/g, ":").split("}").join(",")];
-const SOCIAL = [({{ site.data.social }}).replace(/=>/g, ":").split("}").join(",")];
+const PROJECT = [];
+const SOCIAL = [];
 
 globalThis._PROJECT = PROJECT;
 globalThis._SOCIAL = SOCIAL;
