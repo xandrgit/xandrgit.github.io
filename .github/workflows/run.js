48d48e4e03ad5af2836c31c3b917e2c21144afab
Alexander Antonov <biz.showpozitif5@gmail.com>
2020-11-12 14:45:10 +0300

    Update run.js

--- .github/workflows/run.js
+++ .github/workflows/run.js
@@ -2,9 +2,10 @@ const fs = require("fs");
 const projectData = require(process.cwd() + "/_data/project.json");
 
 if(projectData.length !== 0) {
-  // Data
-  console.log(process.cwd() + "/anxgit/");
-};
-else {
-  // fs.writeFile("file", "No projects at the moment", (err) => { console.error(err); });
+  projectData.map((prj, i) => {
+    const Data = `### <a href="${prj.url}">${prj.name}</a>\${prj.desc}\n\n`;
+    fs.appendFile(process.cwd() + "/anxgit/PROJECTS.md", Data, (err) => { console.error(err); });
+  });
+} else {
+  fs.writeFile(process.cwd() + "/anxgit/PROJECTS.md", "No projects at the moment", (err) => { console.error(err); });
 };
